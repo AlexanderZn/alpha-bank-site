d6e50fc334001cbb42933f446c93028a3ef2c363
Radaga5t <ov.zhen@gmail.com>
2018-02-25 19:37:45 +0700

    Adding 'Specialization' to models

--- RecruitSite/mainApp/models.py
+++ RecruitSite/mainApp/models.py
@@ -10,20 +10,20 @@ class Sphere_Company(models.Model):
 
 class Company(models.Model):
 
-    id_Sphere = models.ForeignKey(Sphere_Company, on_delete=models.CASCADE)
+    sphere = models.ForeignKey(Sphere_Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=45)
 
 
 class Branch(models.Model):
 
-    id_Company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
     info = models.CharField(max_length=100, default='Person')
 
 
 class Subdivision(models.Model):
 
-    id_Branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
     name = models.CharField(max_length=45)
 
 
@@ -32,11 +32,17 @@ class Sphere_Person(models.Model):
     name = models.CharField(max_length=45)
 
 
+class Specialization(models.Model):
+
+    name = models.CharField(max_length=45)
+    sphere = models.ForeignKey(Sphere_Person, on_delete=models.CASCADE)
+
+
 class Person(models.Model):
 
     name = models.CharField(max_length=45)
     lastname = models.CharField(max_length=45)
-    patronymic_Person = models.CharField(max_length=45)
+    patronymic = models.CharField(max_length=45)
     male = models.BooleanField(default=1)
     age = models.IntegerField(default=18)
     phone = models.CharField(max_length=45)
@@ -51,9 +57,9 @@ class Person(models.Model):
 
 class Story(models.Model):
 
-    id_Subdivision = models.ForeignKey(Subdivision, on_delete=models.CASCADE)
-    id_Sphere = models.ForeignKey(Sphere_Person, on_delete=models.CASCADE)
-    id_Person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    date_update = models.DateTimeField(default=timezone.now())
+    subdivision = models.ForeignKey(Subdivision, on_delete=models.CASCADE)
+    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE, null=True)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    date_update = models.DateTimeField(default=timezone.now)
 
 
